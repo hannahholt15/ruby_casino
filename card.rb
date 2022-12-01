6e181ecec3c1585e30747aa75f46067a40cdab5e
hannah holt <hannahholt34@gmail.com>
2022-12-01 16:44:57 -0700

    finished

--- card.rb
+++ card.rb
@@ -1,31 +1,30 @@
-
-require_relative "deck"
 class Card
-
+    # Getter and Setter methods for rank, suit and color
     attr_accessor :rank, :suit, :color
-    def initialize (rank,suit,color)
-        @rank = rank
-        @suit = suit
-        @color = color
+    # Gets called when you call the new method to create an instance
+    # card = Card.new('10', 'Spades', 'Black')
+    def initialize(rank, suit, color)
+      @rank = rank
+      @suit = suit
+      @color = color
     end
-
+    
     def to_i
-        case @rank
-        when 'A'
-            return 1
-        when 'J'
-            return 11
-        when 'Q'
-            return 12
-        when 'K'
-            return 13
-        else
-            @rank.to_i
-        end
-    end
-
-    def to_s
-        return "#{@rank} #{@suit} (#{@color})"
-    end
-
-end 
+      case @rank
+      when 'A'
+        return 1
+      when 'J'
+        return 11
+      when 'Q'
+        return 12
+      when 'K'
+        return 13
+      else 
+        @rank.to_i
+      end
+   end
+   
+   def to_s
+    return "#{@rank}, #{@suit}, (#{@color})"
+   end
+  end
--- player.rb
+++ player.rb
@@ -1,11 +1,8 @@
-
-
 class Player
     attr_accessor :name, :money
-
-    def initialize(name, money)
-        puts "What is your name?"
-        @name = gets.strip
-        @money = 100.0
+    def initialize
+      puts "what is your name?"
+      @name = gets.strip
+      @money = 100.0
     end
-end
+  end 
